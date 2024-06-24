0eba98a969327a3e94d30746ec61c88e762b2d4b
Shreif Shouman <66300695+Shreif-Shouman@users.noreply.github.com>
2024-06-24 11:16:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def load_csv_data(csv_file_path):
     return video_data
 
 def get_conversation_chain(vectorstore):
-    #api_key = os.environ['HUGGINGFACEHUB_API_TOKEN'] = st.secrets['HUGGINGFACEHUB_API_TOKEN']
-    llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2")
+    api_key = os.environ['HUGGINGFACEHUB_API_TOKEN'] = st.secrets['HUGGINGFACEHUB_API_TOKEN']
+    llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2", api_key=api_key)
     conversation_chain = RetrievalQAWithSourcesChain.from_chain_type(
         llm=llm,
         chain_type="stuff",
